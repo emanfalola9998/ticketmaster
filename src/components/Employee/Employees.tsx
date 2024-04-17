2c640be8bd9185f27bd784a29535b0cfe9c1ed49
emmanuelfalola <emmanuelfalola9998u@gmail.com>
2024-04-17 12:28:26 +0100

    scss applied

--- src/components/Employee/Employees.tsx
+++ src/components/Employee/Employees.tsx
@@ -25,22 +25,28 @@ const Employees = ({ employees }: EmployeeProps) => {
   };
 
   return (
-    <div className="employee-container">
-      <h1 className="employee-container__heading">Ticket Tracker</h1>
+    <div className="employee">
+      <h1 className="employee__heading">Ticket Tracker</h1>
+      <div className="employee__content">
       {employees.map((employee: Employee, index: number) => (
-        <div key={employee.id} className="employee-card">
-          <h3 className="employee-card__name">{employee.name}</h3>
-          <h3 className="employee-card__role">{employee.role}</h3>
-          <div className="employee-card__counter">
-            <h3 className="employee-card__counter-heading">Counter</h3>
+        <div key={employee.id} className="employee__content-card">
+          <h3 className="employee__content-card__name">{employee.name}</h3>
+          <h3 className="employee__content-card__role">{employee.role}</h3>
+          <div className="employee__content-card__counter">
+            <h3 className="employee__content-card__counter-heading">Counter</h3>
             {counters[index]}
-            <button onClick={() => handleDecrement(index)}>-</button>
-            <button onClick={() => handleIncrement(index)}>+</button>
+            <div className="employee__content-card__counter-buttons">
+            <button className="employee__content-card__counter-button" onClick={() => handleDecrement(index)}>-</button>
+            <button className="employee__content-card__counter-button" onClick={() => handleIncrement(index)}>+</button>
+            </div>
           </div>
         </div>
       ))}
+      </div>
     </div>
   );
 };
 
 export default Employees;
+
+
