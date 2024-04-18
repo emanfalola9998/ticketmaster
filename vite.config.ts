42f0bf0a740807d2a6ea1b087af11b5f02c8152c
emmanuelfalola <emmanuelfalola9998u@gmail.com>
2024-04-18 10:14:33 +0100

    deployment debugging

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from "url";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(new URL("index.html", import.meta.url)),
+      },
+    },
+  },
   base: "/ticketmaster/"
 })
+
